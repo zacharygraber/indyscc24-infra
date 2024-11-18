feb887285c4440b28d4cc636ba07211d6a42ba7d
Zach Graber <zachgraber27@gmail.com>
2024-11-18 15:51:27 -0500

    reset cpu node flavor

--- compute.tf
+++ compute.tf
@@ -21,7 +21,7 @@ resource "openstack_compute_instance_v2" "cpu_nodes" {
 
     name = "${var.team_name}-cpu${count.index}"
     image_name = "snapshot-${var.team_name}-cpu"
-    flavor_name = "m3.large"
+    flavor_name = "m3.xl"
     key_pair = "indyscc-admins"
 
     user_data = templatefile("cloud-init.yml.tftpl", {
